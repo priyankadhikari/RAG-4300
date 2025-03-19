898862f6154905d10aad999ccf1520091adb3ed0
nidhibendre <bendre.n@northeastern.edu>
2025-03-19 15:13:26 -0400

    updated text_preprocessing.py

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -1,7 +1,51 @@
 from pypdf import PdfReader
+import string
 
-reader = PdfReader("example.pdf")
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()
-print(text)
+with open("english.txt", "r") as f:
+    STOPWORDS = set(line.strip() for line in f)
+
+def get_text(pdf_path):
+    pdf = PdfReader(pdf_path)
+    text_by_page = []
+    for page_num, page in enumerate(pdf.pages):
+        cleaned_text = text_prep(page.extract_text(layout_mode='loose'))
+        text_by_page.append((page_num, cleaned_text))
+    return text_by_page
+
+def text_prep(text, remove_whitespace=True, remove_punctuation=True, remove_stopwords=True):
+    text = text.lower()
+
+    if remove_whitespace:
+        text = text.strip()
+        text = text.replace("\n", " ")
+
+    if remove_punctuation:
+        text = text.translate(str.maketrans("", "", string.punctuation))
+
+    if remove_stopwords:
+        words = text.split()
+        filtered_words = []
+        for word in words:
+            if word not in STOPWORDS:
+                filtered_words.append(word)
+        text = " ".join(filtered_words)
+
+    return text
+
+def split_chunks(text, chunk_size=50, overlap=0):
+    words = text.split()
+    chunks = []
+    for i in range(0, len(words), chunk_size - overlap):
+        chunk = " ".join(words[i: i + chunk_size])
+        chunks.append(chunk)
+    return chunks
+
+def main():
+    pdf_path = "Practical 2.pdf"
+    #print(stopwords.words('english')[:10])
+
+    text_page = get_text(pdf_path)
+
+    print(text_page)
+
+main()
